3e04867c8259cb88dfb08a8ae7f3fe1a9fd9b1ec
Joshua Taylor <taylor.joshua88@gmail.com>
2018-05-13 19:17:18 -0700

    Successfully deployed to Azure

--- WikiWikiWiki/WikiWikiWiki/Program.cs
+++ WikiWikiWiki/WikiWikiWiki/Program.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WikiWikiWiki.Data;
 
 namespace WikiWikiWiki
 {
@@ -14,7 +17,25 @@ namespace WikiWikiWiki
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            IWebHost host = BuildWebHost(args);
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+
+                try
+                {
+                    Task articleSeeding = SeedArticles.Initialize(services);
+                    while (!articleSeeding.IsCompleted) { }
+                }
+                catch
+                {
+                    Console.Error.WriteLine("Could not seed the database with initial articles!");
+                    throw;
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
